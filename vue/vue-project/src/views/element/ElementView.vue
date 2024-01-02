69458e206320939eadf4f810389fac5716fe43e5
MingsonZheng <mingsonzheng@outlook.com>
2024-01-02 16:25:13 +0800

    Element-组件-Fom表单

--- vue/vue-project/src/views/element/ElementView.vue
+++ vue/vue-project/src/views/element/ElementView.vue
@@ -36,6 +36,37 @@
             <el-table-column property="address" label="地址"></el-table-column>
         </el-table>
     </el-dialog>
+
+    <!-- Dialog对话框-Form表单 -->
+    <el-button type="text" @click="dialogFormVisible = true">打开嵌套Form的 Dialog</el-button>
+        
+    <el-dialog title="Form表单" :visible.sync="dialogFormVisible">
+        <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="活动名称">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="活动区域">
+                <el-select v-model="form.region" placeholder="请选择活动区域">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="活动时间">
+                <el-col :span="11">
+                <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+                </el-col>
+                <el-col class="line" :span="2">-</el-col>
+                <el-col :span="11">
+                <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+                </el-col>
+            </el-form-item>
+
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">立即创建</el-button>
+                <el-button>取消</el-button>
+            </el-form-item>
+        </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -43,6 +74,12 @@
 export default {
   data() {
     return {
+      form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: ''
+        },
       gridData: [{
           date: '2016-05-02',
           name: '王小虎',
@@ -61,6 +98,7 @@ export default {
           address: '上海市普陀区金沙江路 1518 弄'
         }],
       dialogTableVisible: false,
+      dialogFormVisible: false,
       tableData: [
         {
           date: "2016-05-02",
@@ -87,10 +125,13 @@ export default {
   },
   methods: {
     handleSizeChange:function(val){
-        alert("每页记录数变化" + val)
+        alert("每页记录数变化" + val);
     },
     handleCurrentChange:function(val){
-        alert("页码发生变化" + val)
+        alert("页码发生变化" + val);
+    },
+    onSubmit:function(){
+        alert(JSON.stringify(this.form));
     }
   }
 };
