f224d17b159d7ae47c528fe73ecdec45cf3d419d
MingsonZheng <mingsonzheng@outlook.com>
2024-01-03 22:45:50 +0800

    maven-依赖管理-依赖范围

--- Web/zzm_web_project/maven-project01/src/main/java/com/zzm/App.java
+++ Web/zzm_web_project/maven-project01/src/main/java/com/zzm/App.java
@@ -1,5 +1,7 @@
 package com.zzm;
 
+//import org.slf4j.Logger;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+//        Logger log;
         System.out.println( "Hello World!" );
     }
 }
---
+++ Web/zzm_web_project/maven-project01/src/test/java/com/zzm/DemoTest.java
@@ -0,0 +1,13 @@
+package com.zzm;
+
+import org.slf4j.Logger;
+
+/**
+ * @author Mingson
+ * @version 1.0
+ */
+public class DemoTest {
+    public void test1() {
+        Logger log;
+    }
+}
