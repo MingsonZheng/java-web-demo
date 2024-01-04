7ccc3463fe01ffe0d1b7741846b9cf115a6f1ee2
MingsonZheng <mingsonzheng@outlook.com>
2024-01-04 14:17:31 +0800

    maven-依赖管理-生命周期

--- Web/zzm_web_project/maven-project01/src/test/java/com/zzm/DemoTest.java
+++ Web/zzm_web_project/maven-project01/src/test/java/com/zzm/DemoTest.java
@@ -1,13 +1,27 @@
 package com.zzm;
 
-import org.slf4j.Logger;
+//import org.slf4j.Logger;
+
+import org.junit.Test;
 
 /**
  * @author Mingson
  * @version 1.0
  */
 public class DemoTest {
+    @Test
     public void test1() {
-        Logger log;
+//        Logger log;
+        System.out.println("hello Test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("hello Test2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("hello Test3");
     }
 }
