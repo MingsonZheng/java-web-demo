78589ba8cc48f3547087fd730579015cd0b9780c
MingsonZheng <mingsonzheng@outlook.com>
2024-01-08 22:43:32 +0800

    Mybatis-基础操作-新增

--- Web/zzm_web_project/springboot-mybatis-crud/src/main/java/com/zzm/mapper/EmpMapper.java
+++ Web/zzm_web_project/springboot-mybatis-crud/src/main/java/com/zzm/mapper/EmpMapper.java
@@ -1,6 +1,8 @@
 package com.zzm.mapper;
 
+import com.zzm.pojo.Emp;
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -9,6 +11,11 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface EmpMapper {
+
+    // 新增员工
+    @Insert("insert into emp (username, name, gender, image, job, entrydate, dept_id, create_time, update_time)" +
+            "values (#{username},#{name},#{gender},#{image},#{job},#{entrydate},#{deptId},#{createTime},#{updateTime})")
+    public void insert(Emp emp);
 
     // 根据ID删除数据
     // 执行SQL时，会将#{...}替换为?，生成预编译SQL，会自动设置参数值。
--- Web/zzm_web_project/springboot-mybatis-crud/src/test/java/com/zzm/SpringbootMybatisCrudApplicationTests.java
+++ Web/zzm_web_project/springboot-mybatis-crud/src/test/java/com/zzm/SpringbootMybatisCrudApplicationTests.java
@@ -1,15 +1,37 @@
 package com.zzm;
 
 import com.zzm.mapper.EmpMapper;
+import com.zzm.pojo.Emp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SpringbootMybatisCrudApplicationTests {
 
     @Autowired
     private EmpMapper empMapper;
+
+    @Test
+    public void testInsert() {
+        // 构造员工对象
+        Emp emp = new Emp();
+        emp.setUsername("Tom");
+        emp.setName("汤姆");
+        emp.setImage("1.jpg");
+        emp.setGender((short)1);
+        emp.setJob((short)1);
+        emp.setEntrydate(LocalDate.of(2000, 1, 1));
+        emp.setCreateTime(LocalDateTime.now());
+        emp.setUpdateTime(LocalDateTime.now());
+        emp.setDeptId(1);
+
+        // 执行新增员工信息操作
+        empMapper.insert(emp);
+    }
 
     @Test
     public void testDelete() {
