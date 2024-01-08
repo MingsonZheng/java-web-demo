a876476af6a6949d114f1f27b855fec1204a8fea
MingsonZheng <mingsonzheng@outlook.com>
2024-01-08 22:26:04 +0800

    Mybatis-基础操作-删除(预编译SQL)

--- Web/zzm_web_project/springboot-mybatis-crud/src/main/java/com/zzm/mapper/EmpMapper.java
+++ Web/zzm_web_project/springboot-mybatis-crud/src/main/java/com/zzm/mapper/EmpMapper.java
@@ -11,6 +11,13 @@ import org.apache.ibatis.annotations.Mapper;
 public interface EmpMapper {
 
     // 根据ID删除数据
+    // 执行SQL时，会将#{...}替换为?，生成预编译SQL，会自动设置参数值。
+    // 使用时机:参数传递，都使用#{...}
+    // Preparing: delete from emp where id = ?
     @Delete("delete from emp where id = #{id}")
+    // 拼接SQL。直接将参数拼接在SQL语句中，存在SQL注入问题。
+    // 使用时机:如果对表名、列表进行动态设置时使用
+    // Preparing: delete from emp where id = 1
+    // @Delete("delete from emp where id = ${id}")
     public void delete(Integer id);
 }
