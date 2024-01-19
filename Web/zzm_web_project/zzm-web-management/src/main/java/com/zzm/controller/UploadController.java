7d68ad26798d3f91c9f957abfaa9f6c8c482df70
MingsonZheng <mingsonzheng@outlook.com>
2024-01-19 18:37:57 +0800

    案例-文件上传-本地存储

--- Web/zzm_web_project/zzm-web-management/src/main/java/com/zzm/controller/UploadController.java
+++ Web/zzm_web_project/zzm-web-management/src/main/java/com/zzm/controller/UploadController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.util.UUID;
+
 /**
  * @author Mingson
  * @version 1.0
@@ -15,8 +18,21 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadController {
 
     @PostMapping("/upload")
-    public Result upload(String username, Integer age, MultipartFile image) {
+    public Result upload(String username, Integer age, MultipartFile image) throws Exception {
         log.info("文件上传：{}, {}, {}", username, age, image);
+
+        // 获取原始文件名
+        String originalFilename = image.getOriginalFilename();
+
+        // 构造唯一的文件名 (不能重复) - uuid(通用唯一识别码) e8ce1104-2bc7-4f27-850f-26d6df7bbc7c
+        int index = originalFilename.lastIndexOf(".");
+        String extname = originalFilename.substring(index);
+        String newFileName = UUID.randomUUID() + extname;
+        log.info("新的文件名：{}", newFileName);
+
+        // 将文件存储在服务器的做盘目录中 D:\Workspace\image
+        image.transferTo(new File("D:\\Workspace\\image\\" + newFileName));
+
         return Result.success();
     }
 }
--- Web/zzm_web_project/zzm-web-management/src/test/java/com/zzm/ZzmWebManagementApplicationTests.java
+++ Web/zzm_web_project/zzm-web-management/src/test/java/com/zzm/ZzmWebManagementApplicationTests.java
@@ -3,11 +3,21 @@ package com.zzm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 @SpringBootTest
 class ZzmWebManagementApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testUuid() {
+        for (int i = 0; i < 1000; i++) {
+            String uuid = UUID.randomUUID().toString();
+            System.out.println(uuid);
+        }
     }
 
 }
