ffff251a65613243593c3653a3e0a10091e3e1e9
MingsonZheng <mingsonzheng@outlook.com>
2024-01-19 15:17:04 +0800

    案例-员工管理-分页查询-分析

---
+++ Web/zzm_web_project/zzm-web-management/src/main/java/com/zzm/pojo/PageBean.java
@@ -0,0 +1,21 @@
+package com.zzm.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * 分页查询结果封装类
+ *
+ * @author Mingson
+ * @version 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageBean {
+    private Long total;// 总记录数
+    private List rows;// 数据列表
+}
