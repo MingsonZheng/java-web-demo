4f5bbd26e754f10954629d819d70496da6071a4d
MingsonZheng <mingsonzheng@outlook.com>
2024-01-21 18:18:28 +0800

    异常处理

---
+++ Web/zzm_web_project/zzm-web-management/src/main/java/com/zzm/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.zzm.exception;
+
+import com.zzm.pojo.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author Mingson
+ * @version 1.0
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class) //捕获所有异常
+    public Result ex(Exception ex) {
+        ex.printStackTrace();
+        return Result.error("对不起，操作失败，请联系管理员");
+    }
+}
