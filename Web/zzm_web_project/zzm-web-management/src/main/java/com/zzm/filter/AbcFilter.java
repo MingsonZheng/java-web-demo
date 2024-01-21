117a0d9c3253a72690cc9e3564fc01188449b32c
MingsonZheng <mingsonzheng@outlook.com>
2024-01-21 17:23:15 +0800

    登录校验-Filter-详解(过滤器链)

---
+++ Web/zzm_web_project/zzm-web-management/src/main/java/com/zzm/filter/AbcFilter.java
@@ -0,0 +1,24 @@
+package com.zzm.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+/**
+ * @author Mingson
+ * @version 1.0
+ */
+@WebFilter(urlPatterns = "/*")
+public class AbcFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Abc 拦截到了请求...放行前逻辑");
+
+        // 放行
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        System.out.println("Abc 拦截到了请求...放行后逻辑");
+
+    }
+}
--- Web/zzm_web_project/zzm-web-management/src/main/java/com/zzm/filter/DemoFilter.java
+++ Web/zzm_web_project/zzm-web-management/src/main/java/com/zzm/filter/DemoFilter.java
@@ -19,12 +19,12 @@ public class DemoFilter implements Filter {
 
     @Override //拦截到请求之后调用，调用多次
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("拦截到了请求...放行前逻辑");
+        System.out.println("Demo 拦截到了请求...放行前逻辑");
 
         // 放行
         filterChain.doFilter(servletRequest, servletResponse);
 
-        System.out.println("拦截到了请求...放行后逻辑");
+        System.out.println("Demo 拦截到了请求...放行后逻辑");
     }
 
     @Override //销毁方法，只调用一次
